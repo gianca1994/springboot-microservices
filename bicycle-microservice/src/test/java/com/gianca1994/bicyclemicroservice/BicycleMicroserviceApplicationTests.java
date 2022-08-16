ff640dcf4d6bc9c4aa5adaeea07a9042ffc3d031
gianca <gianca9405@gmail.com>
2022-08-16 10:43:03 -0300

    bicycle-service add initial project

---
+++ bicycle-microservice/src/test/java/com/gianca1994/bicyclemicroservice/BicycleMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianca1994.bicyclemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
