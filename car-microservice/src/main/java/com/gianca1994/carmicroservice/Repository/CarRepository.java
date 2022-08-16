343e3cff2c81a9122e2b9f82fd083b0101743b24
gianca <gianca9405@gmail.com>
2022-08-16 10:11:56 -0300

    car-service Add repository for cars

---
+++ car-microservice/src/main/java/com/gianca1994/carmicroservice/Repository/CarRepository.java
@@ -0,0 +1,9 @@
+package com.gianca1994.carmicroservice.Repository;
+
+import com.gianca1994.carmicroservice.model.Car;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends MongoRepository<Car, String> {
+}
